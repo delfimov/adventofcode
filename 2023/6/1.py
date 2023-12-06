10cbd5c5eeb2bd94b6e38a11b8337c7265ebc2a2
de <delfimov@1tv.com>
2023-12-06 17:21:23 +0300

    2023: day 6

---
+++ 2023/6/1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import re, math
+
+sum = 1
+input = []
+with open('1.txt') as file:
+    for line in file:
+        line = line.strip()
+        input.append([int(x) for x in re.split(r' +', line.rstrip())[1:]])
+for i, t in enumerate(input[0]):
+    d = input[1][i]
+    d1 = int(math.floor((t - math.sqrt(t*t-4*d))/2)+1)
+    d2 = int(math.ceil((t + math.sqrt(t*t-4*d))/2)-1)
+#    print(t, d, d1, d2, d2-d1+1)
+    sum *= d2-d1+1
+print(sum)
